f7cd58f0ddd1062bcd8736f3fadfd1316b842876
Blake Lein <blake.lein@gmail.com>
2022-06-21 17:55:53 -0500

    End of day commit

--- Algorithms/algorithms.py
+++ Algorithms/algorithms.py
@@ -6,7 +6,6 @@
 # You can return the answer in any order.
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output =[]
         for i in range(len(nums)):
             for j in range(i, len(nums)):
                 if i == j:
