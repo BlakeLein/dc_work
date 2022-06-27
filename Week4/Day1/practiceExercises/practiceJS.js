fb50e4a5daf7541a4545afc279eca4b2862c3ffd
Blake Lein <blake.lein@gmail.com>
2022-06-27 10:39:01 -0500

    mid class

--- Week4/Day1/practiceExercises/practiceJS.js
+++ Week4/Day1/practiceExercises/practiceJS.js
@@ -47,6 +47,37 @@
 // Write a function totalAmount that takes the same arguments as tipAmount except it returns the total as the tip amount plus the bill amount. This function may make use of tipAmount as a sub-task
 // I did this above.
 
+// Exercise 3B
+// const splitAmount = (bill, service, people) => {
+//   if (service === "good") {
+//     bill += bill * 0.2;
+//   } else if (service === "fair") {
+//     bill += bill * 0.15;
+//   } else if (service === "bad") {
+//     bill += bill * 0.1;
+//   }
+//   return bill / people;
+// };
+
+// console.log(splitAmount(20, "good", 6));
+
+// Hello You!
+// const hello = (name) => {
+//   console.log(`Hello ${name}!`);
+// };
+
+// hello("Blake");
+
+// Hello You with default:
+// const hello = (name) => {
+//   if (name) {
+//     console.log(`Hello ${name}!`);
+//   } else {
+//     console.log(`Hello world!`);
+//   }
+// };
+// hello();
+
 // Print Numbers
 // Write a function printNumbers which is given a start number and an end number. It will print the numbers from one to the other, one per line:
 
@@ -71,13 +102,13 @@
 //Exercise 4
 // Write a function printSquare which is given a size and prints a square of that size using asterisks.
 // Old way
-function printSquare(size) {
-  for (let i = 1; i <= size; i++) {
-    let box = "";
-    for (let j = 1; j <= size; j++) {
-      console.log("*");
-    }
-    console.log("*");
-  }
-}
-printSquare(6);
+// function printSquare(size) {
+//   for (let i = 1; i <= size; i++) {
+//     let box = "";
+//     for (let j = 1; j <= size; j++) {
+//       console.log("*");
+//     }
+//     console.log("*");
+//   }
+// }
+// printSquare(6);
