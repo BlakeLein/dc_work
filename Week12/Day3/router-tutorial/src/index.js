d8bb6e6b0ce33f351a6a43213ca3507e307b0ce7
Blake Lein <blake.lein@gmail.com>
2022-08-25 08:26:00 -0500

    front or back challenge

---
+++ Week12/Day3/router-tutorial/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
